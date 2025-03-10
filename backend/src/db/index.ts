7e2d2be6d19322d5ecc499d52fc4884cf6f3a128
matter <moondragon@lbtil.com>
2025-03-10 09:21:40 -0500

    Removed hard code

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'btiflix',
-  password: process.env.DB_PASSWORD || 'BTIonelove',
+  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
 });
 
